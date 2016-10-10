d4ef96d7089df95f66cc4c8cf0ed29782978beb6
Adrian Lee <leeadrian841@gmail.com>
2016-10-10 12:17:17 +0800

    first

--- car.js
+++ car.js
@@ -2,8 +2,9 @@ var Car = function (speed, color, convertible) {
   this.speed = speed
   this.color = color
   this.convertible = convertible
-  this.accelerate = function (accelerate) {
-    this.speed += accelerate
+  this.accelerate = function (num) {
+    this.speed += num
+    return speed
   }
   this.setColor = function (newColor) {
     this.color = newColor
